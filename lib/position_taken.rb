8ca6f92935461f7ebd0da7c8d4325f90d106ce4f
Donat Pllana <donat.pllana@outlook.com>
2020-05-15 05:16:32 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
-  if (board[index] == nil)
+  if board[index] == " " || board[index] == "" || board[index]==nil
     false
   elsif board[index] == "X" || board[index] == "O"
     true
-  elsif board[index] == " " || board[index] == ""
-    false
   end
 end
