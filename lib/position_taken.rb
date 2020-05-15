577344d2cbc4c922be3dd9d8e6b3cf2e77f96208
Donat Pllana <donat.pllana@outlook.com>
2020-05-15 05:08:43 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken
+  if board[index] == " " || board[index] == ""
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
